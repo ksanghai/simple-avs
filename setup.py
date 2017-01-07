e6a87942f1d35035104ee5b00caeb6c503cbfb54
Rob Ladbrook <robl@theappbusiness.com>
2017-01-07 23:59:14 +0000

    Add PyPI setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+""" Register on PyPI """
+from distutils.core import setup
+
+
+setup(
+    name='simpleavs',
+    packages=['simpleavs'],
+    version='0.1',
+    description='Simple AVS API Client (Amazon Alexa Voice Service) for v20160207',
+    author='Rob Ladbrook',
+    author_email='simpleavs@slyfx.com',
+    url='https://github.com/robladbrook/simpleavs',
+    download_url='https://github.com/robladbrook/simpleavs/tarball/0.1',
+    keywords=['avs', 'alexa', 'amazon', 'voice'],
+    classifiers=[],
+)
