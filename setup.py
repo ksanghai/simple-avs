eb30767d28e023244ff90592af2a931d5139e46a
Rob Ladbrook <robl@theappbusiness.com>
2017-01-08 00:39:15 +0000

    Slight repo rename

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 setup(
     name='simpleavs',
     packages=['simpleavs'],
-    version='0.3',
+    version='0.3.1',
     description='Simple AVS API Client (Amazon Alexa Voice Service) for v20160207',
     author='Rob Ladbrook',
     author_email='simpleavs@slyfx.com',
-    url='https://github.com/robladbrook/simpleavs',
-    download_url='https://github.com/robladbrook/simpleavs/tarball/0.3',
+    url='https://github.com/robladbrook/simple-avs',
+    download_url='https://github.com/robladbrook/simple-avs/tarball/0.3.1',
     keywords=['avs', 'alexa', 'amazon', 'voice'],
     classifiers=[],
     install_requires=['pyyaml', 'requests', 'hyper']
