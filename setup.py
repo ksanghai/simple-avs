8f6c12da853b737c21b6df166b2e6fe55a4b9616
Rob Ladbrook <robl@theappbusiness.com>
2017-01-08 00:29:48 +0000

    Replace distutils for setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 """ Register on PyPI """
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='simpleavs',
     packages=['simpleavs'],
-    version='0.2',
+    version='0.3',
     description='Simple AVS API Client (Amazon Alexa Voice Service) for v20160207',
     author='Rob Ladbrook',
     author_email='simpleavs@slyfx.com',
     url='https://github.com/robladbrook/simpleavs',
-    download_url='https://github.com/robladbrook/simpleavs/tarball/0.2',
+    download_url='https://github.com/robladbrook/simpleavs/tarball/0.3',
     keywords=['avs', 'alexa', 'amazon', 'voice'],
     classifiers=[],
     install_requires=['pyyaml', 'requests', 'hyper']
