e5d4f1cdb2165e90f3d0983d2f42fa1fafddf2c1
Rob Ladbrook <robl@theappbusiness.com>
2017-01-08 00:18:06 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Rob Ladbrook',
     author_email='simpleavs@slyfx.com',
     url='https://github.com/robladbrook/simpleavs',
-    download_url='https://github.com/robladbrook/simpleavs/tarball/0.1',
+    download_url='https://github.com/robladbrook/simpleavs/tarball/0.2',
     keywords=['avs', 'alexa', 'amazon', 'voice'],
     classifiers=[],
     install_requires=['pyyaml', 'requests', 'hyper']
